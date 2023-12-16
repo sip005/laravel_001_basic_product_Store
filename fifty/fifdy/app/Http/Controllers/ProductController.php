f5d010bcea7d3030d3cf4f912afbae08ac7ad905
piranha <sip08101996@gmail.com>
2023-12-17 02:01:46 +0600

    done
    need to add styling

---
+++ fifty/fifdy/app/Http/Controllers/ProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    // Return view out to the user
+//    public function index(){
+    //        return view('products.index');
+        public function index(){
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+//        dd($request->name);
+//    }
+        $data = $request->validate([
+          'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+
+        public function edit(Product $product){
+//            dd($product);
+            return view('products.edit', ['product' => $product]);
+//
+    }
+
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success', 'Product Updated Succesffully');
+
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect(route('product.index'))->with('success', 'Product deleted Succesffully');
+    }
+
+}
